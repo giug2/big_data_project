659976b6a7a97770abd40161495df2b1b25ef4d0
giug2 <giulia.gichi@gmail.com>
2025-06-07 14:23:18 +0200

    update

--- data/divisione_dataset.py
+++ data/divisione_dataset.py
@@ -1,5 +1,27 @@
 import pandas as pd
 
+# Job 1
+# Percorso al tuo dataset
+dataset_path1 = 'Job 2\job2_dataset.csv'
+
+# Carica il dataset
+df = pd.read_csv(dataset_path1)
+
+# Shuffle per rimescolare i dati prima di fare le divisioni (opzionale)
+df = df.sample(frac=1, random_state=42).reset_index(drop=True)
+
+# Percentuali di benchmark
+percentuali = [0.1, 0.3, 0.5, 0.7]
+
+# Salva ogni benchmark in un file separato
+for p in percentuali:
+    benchmark = df.iloc[:int(len(df) * p)]
+    filename = f'benchmark_{int(p*100)}.csv'
+    benchmark.to_csv(filename, index=False)
+    print(f'Creato {filename} con {len(benchmark)} righe')
+
+
+# Job 2
 # Percorso al tuo dataset
 dataset_path = 'Job 2\job2_dataset.csv'
 
@@ -8,9 +30,6 @@ df = pd.read_csv(dataset_path)
 
 # Shuffle per rimescolare i dati prima di fare le divisioni (opzionale)
 df = df.sample(frac=1, random_state=42).reset_index(drop=True)
-
-# Percentuali di benchmark
-percentuali = [0.1, 0.3, 0.5, 0.7]
 
 # Salva ogni benchmark in un file separato
 for p in percentuali:
